ffeb956bc9961e201a906a74d9877c1499e1f062
Anabell Lopez <ana987bell@hotmail.com>
2022-07-04 15:28:39 -0500

    update final

--- src/components/ItemDetailContainer.js
+++ src/components/ItemDetailContainer.js
@@ -3,18 +3,22 @@ import { ItemDetail } from './ItemDetail';
 
 const ItemDetailContainer = () => {
     const [producto, setproducto] = useState({});
+    const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
+        setIsLoading(true);
         setTimeout(
             () =>
                 fetch('./data/data.json')
                 .then(resp => resp.json())
-                .then((data) => {setproducto(data.find((i) => i.id === 1))})                
+                .then((data) => {setproducto(data.find((i) => i.id === 1))})
+                .finally(() => setIsLoading(false))                
                 , 2000);
     }, [])
 
     return (
-        <ItemDetail producto={producto}/>
+        isLoading ? (<h1>Cargando...</h1>) :
+        (<ItemDetail producto={producto}/>)
     );
     };
 
