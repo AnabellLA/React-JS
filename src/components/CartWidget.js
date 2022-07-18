ea32ade5d0bb7ca8bb5648d9b52bea5d24fdfd81
Anabell Lopez <ana987bell@hotmail.com>
2022-07-18 15:57:12 -0500

    Update cart

--- src/components/CartWidget.js
+++ src/components/CartWidget.js
@@ -1,12 +1,37 @@
-import cart from '../shopping-cart.png';
+import React, { useContext, useEffect, useState } from 'react';
+import cartIcon from '../shopping-cart.png';
 import './CartWidget.css';
 import { Link } from 'react-router-dom';
 import { Nav } from 'react-bootstrap';
+import { CartContext } from "./Context/CartContext";
+
 
 function CartWidget(){
-    return(
-        <Nav.Link as={Link} to={"/cart"}><img src={ cart } alt="Cart Widget" id="cart"/></Nav.Link>
-    )
+    const { cart, getQuantity } = useContext(CartContext);
+    const[empty, setEmpty] = useState(true);
+
+    useEffect(() => {
+        if (cart.length === 0){
+            setEmpty(true);
+        }else{
+            setEmpty(false);
+        }
+    }, [cart.length]);
+
+    if(!empty) {
+        return(
+            <div id='carrito'>
+                <Nav.Link as={Link} to={"/cart"}><img src={ cartIcon } alt="Cart Widget" id="cart"/></Nav.Link>
+                <span id='cantidad'>{getQuantity()}</span>
+            </div>
+        )    
+    }else{
+        return(
+            <div>
+                <Nav.Link as={Link} to={"/cart"}><img src={ cartIcon } alt="Cart Widget" id="cart"/></Nav.Link>
+            </div>
+        )    
+    }
 }
 
 export default CartWidget;
